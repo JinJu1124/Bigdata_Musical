20710b8c3f7dbba3385a666f08a1a1fbe8a399d6
JinJu1124 <banggi1124@gmail.com>
2021-11-26 17:07:07 +0900

    url

--- interpark_musical.py
+++ interpark_musical.py
@@ -12,6 +12,8 @@ driver.maximize_window()
 url = "https://tickets.interpark.com/search?keyword=%EB%AE%A4%EC%A7%80%EC%BB%AC"
 driver.get(url)
 
+time.sleep(3)
+
 #티켓 카테고리로 넘어가기
 driver.find_element_by_css_selector("#allContent > div.ticketContent > div.searchHeader > a").click()
 time.sleep(3)
@@ -27,45 +29,31 @@ item_urls = []
 #뮤지컬 리스트 뽑기 눈물난다..
 musical_list = driver.find_element_by_xpath('//*[@class="categoryContent current"]/div[@class="ticketContent"]/div[@class="searchPanel"]')
 for p in musical_list.find_elements_by_tag_name('li'):
-    musical_title = titles.append(p.find_element_by_class_name('itemName').text) #뮤지컬 제목
-    areas.append(p.find_element_by_class_name('area').text)
-    periods.append(p.find_element_by_class_name('period').text)
-    item_urls.append(p.find("a").attrs['href']) # url
+    item_name = p.find_element_by_class_name('itemName') #itemName 요소
+    titles.append(item_name.text) #뮤지컬 제목
+    areas.append(p.find_element_by_class_name('area').text) #지역
+    periods.append(p.find_element_by_class_name('period').text) #기간
+    item_urls.append(item_name.find_element_by_css_selector('a').get_attribute('href')) #url
 
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(0.5)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
 
 #종료하기
 driver.close()
 
-df = pd.DataFrame({'title':titles, 'Area': areas, 'Period': periods, 'URL': item_urls})
-# df = pd.DataFrame({'title':titles})
-print(df)
+df = pd.DataFrame({'title':titles, 'Area': areas, 'Period': periods, 'URL':item_urls})
+df = df.replace('\n', ' ', regex=True)
 
-
-#ticketContent > div > div.searchPanel > ul > li:nth-child(1)
-
-
-# for item in items : 
-#     titles.append(item.find("div", attrs={"class":"itemName"}).get_text())
-
-# items = content.select(re.compile("categoryContent current$"))
-# items = content.select('div.categoryContent current > div.ticketContent > div.searchPanel > ul.itemList')
-
-
-
-
-# # for item in items:
-# #     titles.append(item.find("div", attrs={"class":"itemName"}).get_text()) #뮤지컬 타이틀
-# #     tags.append(item.find("div", attrs={"class":"tags"}).get_text())#태그
-# #     areas.append(item.find("span", attrs={"class":"area"}).get_text())#지역
-# #     periods.append(item.find("span", attrs={"class":"period"}).get_text())#기간
-# #     item_urls.append(item.find("a").attrs['href']) # url
-
-
-
-
-# dataframe = pd.DataFrame({'Title': titles, 'Tag': tags, 'Area': areas, 'Period': periods, 'URL': item_urls})
-
-# # clean DataFrame
-# dataframe = dataframe.replace('\n', ' ', regex=True)
-# print(len(titles))
-# print(dataframe)
+df.to_csv('musical_url.csv')
