65810bb1cf9007168213d73f7132dc457eff96a6
JinJu1124 <banggi1124@gmail.com>
2021-11-29 20:28:39 +0900

    interpark_musical_detail

--- interpark_musical_data.py
+++ interpark_musical_data.py
@@ -4,113 +4,123 @@ import re
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#chrome 드라이버 객체 실행시키기
-driver = webdriver.Chrome("/Users/dindoo/Documents/Bigdata_Musical/chromedriver")
+# chrome 드라이버 객체 실행시키기
+driver = webdriver.Chrome(
+    "/Users/dindoo/Documents/Bigdata_Musical/chromedriver")
 driver.maximize_window()
 
-#홈페이지 열기 interpark 뮤지컬 티켓 부분
+# 홈페이지 열기 interpark 뮤지컬 티켓 부분
 url = "https://tickets.interpark.com/search?keyword=%EB%AE%A4%EC%A7%80%EC%BB%AC"
 driver.get(url)
 time.sleep(3)
 
 
-#티켓 카테고리로 넘어가기
-driver.find_element_by_css_selector("#allContent > div.ticketContent > div.searchHeader > a").click()
+# 티켓 카테고리로 넘어가기
+driver.find_element_by_css_selector(
+    "#allContent > div.ticketContent > div.searchHeader > a").click()
 time.sleep(2)
 
-content= BeautifulSoup(driver.page_source, 'html.parser')
+content = BeautifulSoup(driver.page_source, 'html.parser')
 
-titles = [] #
-genres = [] #
-musical_times = [] #
-ages = [] #
-actors = [] #
-tags = [] #
-areas = [] #
-places = [] #
-periods = [] #
-# musical_poster = [] 
+titles = []
+genres = []
+musical_times = []
+ages = []
+actors = []
+tags = []
+areas = []
+places = []
+periods = []
+itme_urls = []
+# musical_poster = []
 
 
 for n in range(1, 500):
-    #뮤지컬 리스트 뽑기 눈물난다..
-    
-    musical_list = driver.find_element_by_xpath('//*[@class="categoryContent current"]/div[@class="ticketContent"]/div[@class="searchPanel"]')
-    for p in musical_list.find_elements_by_tag_name('li'):
-        item_name = p.find_element_by_class_name('itemName') #itemName 요소     
-        
-        #뮤지컬 제목
-        titles.append(item_name.text)
-        
-        #장르
-        genre = p.find_element_by_css_selector('div.itemInfo > div > span.type').text
-        genres.append(genre)  
+    # 뮤지컬 리스트 뽑기 눈물난다..
 
-        #지역 
+    musical_list = driver.find_element_by_xpath(
+        '//*[@class="categoryContent current"]/div[@class="ticketContent"]/div[@class="searchPanel"]')
+    for p in musical_list.find_elements_by_tag_name('li'):
+        item_name = p.find_element_by_class_name('itemName')  # itemName 요소
+
+        # 뮤지컬 제목
+        titles.append(item_name.text)
+
+        # 장르
+        genre = p.find_element_by_css_selector(
+            'div.itemInfo > div > span.type').text
+        genres.append(genre)
+
+        # 지역
         try:
-            areas.append(p.find_element_by_class_name('area').text) 
+            areas.append(p.find_element_by_class_name('area').text)
         except:
             areas.append(None)
-        
-        #기간
-        try:
-            periods.append(p.find_element_by_class_name('period').text) 
-        except:
-            periods.append(None) #기간  
 
-        #공연 시간
+        # 기간
         try:
-            musical_time = p.find_element_by_css_selector('div.itemInfo > div > span.time').text
+            periods.append(p.find_element_by_class_name('period').text)
+        except:
+            periods.append(None)  # 기간
+
+        # 공연 시간
+        try:
+            musical_time = p.find_element_by_css_selector(
+                'div.itemInfo > div > span.time').text
             musical_times.append(musical_time)
         except:
             musical_times.append(None)
 
-        #연령가
+        # 연령가
         try:
-            age = p.find_element_by_css_selector('div.itemInfo > div > span.visible').text
+            age = p.find_element_by_css_selector(
+                'div.itemInfo > div > span.visible').text
             ages.append(age)
         except:
             ages.append(None)
 
-        #출연진
+        # 출연진
         try:
-            actor = p.find_element_by_css_selector('div.itemInfo > div.castWrap.castDate > span.content').text
+            actor = p.find_element_by_css_selector(
+                'div.itemInfo > div.castWrap.castDate > span.content').text
             actors.append(actor)
         except:
             actors.append(None)
 
-        #tag 판매중 or 종료
+        # tag 
         try:
             tag = p.find_element_by_css_selector('div.tags').text
             tags.append(tag)
-            print(tag)
         except:
             print('error')
-            # tag = "판매 종료"
-            # tags.append(tag)
 
-        #장소
+        # 장소
         try:
-            place = p.find_element_by_css_selector('div.col.info > span.concertHall').text
+            place = p.find_element_by_css_selector(
+                'div.col.info > span.concertHall').text
             places.append(place)
         except:
             places.append(None)
+        
+        itme_urls.append(item_name.find_element_by_css_selector('a').get_attribute('href')) #url
 
-#페이지 넘기기
+# 페이지 넘기기
     page_bar = driver.find_elements_by_css_selector("span.pageNumber > *")
     n = n + 1
-    if n%10 == 0:
+    if n % 10 == 0:
         print(n)
-        driver.find_element_by_css_selector('#ticketContent > div > div.pagination > a.next').click()
-        
+        driver.find_element_by_css_selector(
+            '#ticketContent > div > div.pagination > a.next').click()
+
     else:
-        page_bar[n%10].click()
+        page_bar[n % 10].click()
 
 
-df = pd.DataFrame({'Title':titles, 'Genre':genres, 'Time':musical_times, 'Age':ages, 'Actor':actors, 'Area': areas, 'Place':places, 'Period': periods, 'Tag':tags})
+df = pd.DataFrame({'Title': titles, 'Genre': genres, 'Time': musical_times, 'Age': ages,
+                  'Actor': actors, 'Area': areas, 'Place': places, 'Period': periods, 'Tag': tags, 'URL':itme_urls})
 df = df.replace('\n', ' ', regex=True)
 
 df.to_csv('musical_data.csv')
 
-#종료하기
+# 종료하기
 driver.close()
---
+++ interpark_musical_detail_data.py
@@ -0,0 +1,166 @@
+from selenium import webdriver
+import time
+import re
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# chrome 드라이버 객체 실행시키기
+driver = webdriver.Chrome(
+    "/Users/dindoo/Documents/Bigdata_Musical/chromedriver")
+driver.maximize_window()
+
+data = pd.read_csv(r"musical_url.csv")  # CSV 파일 불러오기
+urls = data['URL'].values.tolist()  # 리스트에 값
+
+titles = []
+musical_times = []
+ages = []
+places = []
+periods = []
+URLs = []
+like_sex_m = []
+like_sex_w = []
+like_age_1 = []
+like_age_2 = []
+like_age_3 = []
+like_age_4 = []
+like_age_5 = []
+ticketCast = []
+musical_status = []
+
+
+# url 리스트 돌며 페이지 열기(sample 10개)
+i = 0
+for url in urls:
+    i = i+1  # 브레이크용도
+
+    # 해당 url 리스트에 저장하기
+    URLs.append(url)
+    try:
+        # url 열기
+        driver.get(url)
+
+        # 쉬기
+        driver.implicitly_wait(10)
+    
+        # 제목
+        titles.append(driver.find_element_by_css_selector('div.productMainTop > div > div.summaryTop > h2').text)
+        
+        #판매취소 작품 거르기
+        try:
+            status = driver.find_element_by_css_selector('#productSide > div > div.sideMain > div > div > div > div > strong').text
+            if status == '판매취소':
+                musical_status.append(status)
+                musical_times.append(None)
+                ages.append(None)
+                places.append(None)
+                periods.append(None)
+                like_sex_m.append(None)
+                like_sex_w.append(None)
+                like_age_1.append(None)
+                like_age_2.append(None)
+                like_age_3.append(None)
+                like_age_4.append(None)
+                like_age_5.append(None)
+                ticketCast.append(None)
+                print('판매 취소')
+
+        except:
+            musical_status.append('판매예정/판매중/판매종료')
+            
+            # 장소
+            try:
+                places.append(driver.find_element_by_css_selector(
+                'div.summaryBody > ul > li:nth-child(1) > div > a').text)
+            except:
+                places.append(None)
+
+            # 공연 시간
+            try:
+                musical_times.append(driver.find_element_by_css_selector(
+                    'div.summaryBody > ul > li:nth-child(2) > div > p').text)
+            except:
+                musical_times.append(None)
+
+            # 공연 기간
+            try:
+                periods.append(driver.find_element_by_css_selector(
+                    'div.summaryBody > ul > li:nth-child(2) > div > p').text)
+            except:
+                periods.append(None)
+
+            # 관람 연령
+            try:
+                ages.append(driver.find_element_by_css_selector(
+                    'div.summaryBody > ul > li:nth-child(4) > div > p').text)
+            except:
+                ages.append(None)
+            
+            # 선호 성별
+            try:
+                like_sex_m.append(driver.find_element_by_css_selector(
+                    'div.statGenderType.is-male > div.statGenderValue').text)  # 남
+            except:
+                like_sex_m.append(None)
+            try:
+                like_sex_w.append(driver.find_element_by_css_selector(
+                    'div.statGenderType.is-female > div.statGenderValue').text)  # 녀
+            except:
+                like_sex_w.append(None)
+
+            # 선호 연령
+            try:
+                like_age_1.append(driver.find_element_by_css_selector(
+                    'div.statAge > div > div:nth-child(1) > div.statAgePercent').text)  # 10대
+            except:
+                like_age_1.append(None)
+            try:
+                like_age_2.append(driver.find_element_by_css_selector(
+                    'div.statAge > div > div:nth-child(2) > div.statAgePercent').text)  # 20대
+            except:
+                like_age_2.append(None)
+            try:
+                like_age_3.append(driver.find_element_by_css_selector(
+                    'div.statAge > div > div:nth-child(3) > div.statAgePercent').text)  # 30대
+            except:
+                like_age_3.append(None)
+            try:
+                like_age_4.append(driver.find_element_by_css_selector(
+                    'div.statAge > div > div:nth-child(4) > div.statAgePercent').text)  # 40대
+            except:
+                like_age_4.append(None)
+            try:
+                like_age_5.append(driver.find_element_by_css_selector(
+                    'div.statAge > div > div:nth-child(5) > div.statAgePercent').text)  # 50대
+            except:
+                like_age_5.append(None)
+
+            # 티켓캐스트
+            try:
+                ticketCast.append(driver.find_element_by_css_selector(
+                    'div.summaryBody > div > div.posterBoxBottom > div.prdCast > p').text)
+            except:
+                ticketCast.append(None)
+            
+
+            
+    except: #페이지가 더이상 유효하지 않을때
+        titles.append(None)
+        print('주의! 페이지가 유효하지 않습니다.')
+    
+    # url sample을 위한 브레이크
+    if i == 10:
+        break
+# print(titles, places, musical_times, ages, periods, ticketCast, like_sex_m, like_sex_w, like_age_1, like_age_2, like_age_3, like_age_4, like_age_5, URLs)
+df = pd.DataFrame({'Title': titles, 'Place': places, 'Time': musical_times, 'Age': ages, 'Period': periods,
+                   'TicketCast': ticketCast, 'Like_man': like_sex_m, 'Like_woman': like_sex_w,
+                   'Like_age_10': like_age_1, 'Like_age_20': like_age_2, 'Like_age_30': like_age_3, 'Like_age_40': like_age_4, 'Like_age_50': like_age_5, 'URL': URLs,
+                   'Status': musical_status})
+
+df = df.replace('\n', ' ', regex=True)
+print(df)
+df.to_csv('musical_detailed_data.csv')
+
+# 종료하기
+driver.close()
